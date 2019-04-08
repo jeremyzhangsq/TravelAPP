818dc426d682bd6355c730f4f75251d60ed5cc48
jeremyzhangsq <jeremyzhangsq@gmail.com>
2019-04-09 00:34:04 +0800

    front end v1.0

--- pages/list/list.js
+++ pages/list/list.js
@@ -3,20 +3,12 @@ Page({
  
   data:{
     artlist: [
-      { art: "1111111111111111111111" },
-      { art: "2222222222222222222222" },
-      { art: "3333333333333333333333" },
-      { art: "4444444444444444444444" },
-      { art: "5555555555555555555555" },
-      { art: "6666666666666666666666" }
-    ],
-    imglist: [
-      { img: "../../icon/People.png" },
-      { img: "../../icon/People.png" },
-      { img: "../../icon/People.png" },
-      { img: "../../icon/People.png" },
-      { img: "../../icon/People.png" },
-      { img: "../../icon/People.png" }
+      { art: "1111111111111111111111", img: "../../icon/People.png" },
+      { art: "2222222222222222222222", img: "../../icon/People.png" },
+      { art: "3333333333333333333333", img: "../../icon/People.png" },
+      { art: "4444444444444444444444", img: "../../icon/People.png" },
+      { art: "5555555555555555555555", img: "../../icon/People.png" },
+      { art: "6666666666666666666666", img: "../../icon/People.png" }
     ],
     focus: true,
     inputValue: ''
