ca5c0c96c4c0d0dd53561dc7ff94e0fc47592c0b
jeremyzhangsq <jeremyzhangsq@gmail.com>
2019-04-29 10:37:42 +0800

    audio add

--- pages/article/article.js
+++ pages/article/article.js
@@ -8,6 +8,9 @@ Page({
   data: {
     pageURL:"https://borischen.cn/static/",
     title:" ",
+    name: '此时此刻',
+    author: '',
+    src: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46',
     id:0
   },
 
@@ -54,7 +57,8 @@ Page({
    * Lifecycle function--Called when page is initially rendered
    */
   onReady: function () {
-
+    // 使用 wx.createAudioContext 获取 audio 上下文 context
+    this.audioCtx = wx.createAudioContext('myAudio')
   },
 
   /**
