8e0271e1508de5624f73453f264d28c6c7dba830
jeremyzhangsq <jeremyzhangsq@gmail.com>
2019-04-29 14:02:41 +0800

    reserve post 1.0

--- pages/reserve/reserve.js
+++ pages/reserve/reserve.js
@@ -8,6 +8,9 @@ Page({
     index: 0,
     multiIndex: [0, 0, 0],
     date: '2019-09-01',
+    name: "",
+    id: '',
+    tel:'',
     color: '#888888'
   },
 
@@ -23,12 +26,56 @@ Page({
   onReady: function () {
 
   },
+  nameInput: function (e) {
+    var value = e.detail.value;
+    console.log(e.detail.value)
+    var that = this
+    that.setData({
+      name: value,
+    });
+  },
+  idInput: function (e) {
+    var value = e.detail.value;
+    console.log(value)
+    var that = this
+    that.setData({
+      id: value,
+    });
+  },
+  telInput: function (e) {
+    this.setData({
+      tel: e.detail.value,
+    });
+  },
   bindDateChange(e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
       date: e.detail.value,
       color: '#000000'
     })
+  },
+  submit:function (e){
+  
+    var that = this //创建一个名为that的变量来保存this当前的值  
+    wx.request({
+      url: 'https://borischen.cn/django/appointment/change_info',
+      method: 'post',
+      data: {
+        name: that.data.name,
+        id_card : that.data.id,
+        phone : that.data.tel,
+        date : that.data.date
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'  //这里注意POST请求content-type是小写，大写会报错  
+      },
+      success: function (res) {
+        // that.setData({ //这里是修改data的值  
+        //   test: res.data //test等于服务器返回来的数据  
+        // });
+        console.log(res.data)
+      }
+    });
   },
   /**
    * Lifecycle function--Called when page show
